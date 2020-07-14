6ad263369f326ae119ccb6be05c81d38a0a4305a
Vitor Moraes <vitormoraes3@yahoo.com.br>
2020-07-13 22:41:02 -0300

    Feat: App pagination

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -5,19 +5,26 @@ import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
 
 const Feed = () => {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  async function loadPage(pageNumber = page) {
+    if (total && pageNumber > total) return;
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
+    );
+
+    const data = await response.json();
+    const totalItens = await response.headers.get('x-Total-Count');
+
+    setTotal(Math.floor(totalItens / 5));
+    setFeed([...feed, ...data]);
+    setPage(pageNumber + 1);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://localhost:3000/feed?_expand=author&_limit=5&_page=1',
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    }
-
-    loadFeed();
+    loadPage();
   }, []);
 
   return (
@@ -25,6 +32,8 @@ const Feed = () => {
       <FlatList
         data={feed}
         keyExtractor={(post) => String(post.id)}
+        onEndReached={() => loadPage()} // Essa função é chamada dessa forma para não receber parâmetros odo onEndRechead
+        onEndReachedThreshold={0.1}
         renderItem={({item}) => (
           <Post>
             <Header>
