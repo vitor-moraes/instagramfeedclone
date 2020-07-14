6b3b376cbe1a3787c680dbd80a118f5005b9f7b3
Vitor Moraes <vitormoraes3@yahoo.com.br>
2020-07-14 09:32:58 -0300

    Feat: LazyImage and changing animation to original

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,15 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList} from 'react-native';
 
-import {
-  Post,
-  Header,
-  Avatar,
-  Name,
-  PostImage,
-  Description,
-  Loading,
-} from './styles';
+import LazyImage from '../../components/LazyImage';
+
+import {Post, Header, Avatar, Name, Description, Loading} from './styles';
 
 const Feed = () => {
   const [feed, setFeed] = useState([]);
@@ -65,7 +59,11 @@ const Feed = () => {
               <Name>{item.author.name}</Name>
             </Header>
 
-            <PostImage ratio={item.aspectRatio} source={{uri: item.image}} />
+            <LazyImage
+              aspectRatio={item.aspectRatio}
+              smallSource={{uri: item.small}}
+              source={{uri: item.image}}
+            />
 
             <Description>
               <Name>{item.author.name}</Name> {item.description}
